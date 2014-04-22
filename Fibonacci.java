d089b72758b75ffc4cdf5d12a5ca90b2e8f15dc1
Fachriannoor <noorfachrian@gmail.com>
2014-04-22 17:38:39 +0700

    Ini yang di modif

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,21 +1,22 @@
-+	import java.util.Scanner;
-+	
-+	public class Fibonacci {
-+	    
-+	    public static void main(String[] args) {
-+	        int a=1,b=0,c=1,d,e,i;
-+	        
-+	        Scanner cin = new Scanner(System.in);
-+	        System.out.println("Masukan angka: ");
-+	        i= cin.nextInt();
-+	        for(int x=0;x<i;x++){
-+	            System.out.print(a+" ");
-+	            a= b + c;
-+	            d=c;
-+	            b=d;
-+	            e=a;
-+	            c=e;
-+	        }
-+	        System.out.println("");
-+	    }
-+	}
+import java.util.Scanner;
+
+public class Fibonacci {
+    
+    public static void main(String[] args) {
+        int a=1,b=0,c=1,d,e,f=0,i;
+        
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Masukan angka: ");
+        i= cin.nextInt();
+        for(int x=0;x<i;x++){
+            f=f+a;
+            a= b + c;
+            d=c;
+            b=d;
+            e=a;
+            c=e;
+        }
+        System.out.println(f);
+    }
+}
+
